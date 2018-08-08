57c8dd47c1437d412a008deff84366aaa403b1bd
Liudmilak <liudmilak@netvertise.com>
2018-08-08 16:54:07 +0300

    50% homework

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// const app = require("express")();
 const express = require("express");
 const app = express();
 const config = require("./config/development");
 const bodyParser = require('body-parser');
+const slug = require('slug');
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
@@ -10,19 +10,19 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 app.use((req, res, next) => {
     console.log(`${req.url} --> ${req.method} --> ${Date.now()}`);
     next();
-})
+});
 
 // endpoint = url + method
 
 const USERS = require("./mock-data/users");
 
-// Controller
+
 const getUsers = (req, res, next) => {
     req.users = USERS;
     next();
 };
 
-// Controller
+
 const sendUsers = (req, res, next) => {
     res.status(200);
     res.json(req.users);
@@ -33,36 +33,80 @@ const addUser = (req, res, next) => {
     USERS.push(user);
     req.users = USERS;
     next();
-}
+};
+
+const deleteUsers = (req, res, next) => {
+  const index = req.params.index;
+  delete USERS[index];
+  req.users = USERS;
+  next();
+};
+
+const putUsers = (req, res, next) => {
+    const user = req.body;
+    const index = req.params.index;
+    const update = Object.assign(USERS[index], user);
+    const final = Object.assign(USERS, update);
+    req.users = final;
+    next();
+};
 
 const getBooks = (req, res, next) => {
     const index = req.params.index;
     req.books = USERS[index].books;
     next();
 };
+
 const sendBooks = (req, res, next) => {
     res.status(200);
     res.json(req.books);
-}
+};
+
+const deleteBooks = (req, res, next) => {
+    const index = req.params.index;
+    const newTitle = slug(USERS[index].books.title);
+
+    req.books = USERS[index].books;
+    next();
+};
+
+const postBooks = (req, res, next) => {
+    const book = req.body;
+    const index = req.params.index;
+    USERS[index].books.push(book);
+    req.books = USERS[index].books;
+    next();
+};
+
+
+// const putBooks = (req, res, next) => {
+//     const book = req.body;
+//     const index = req.params.index;
+//     const update = Object.assign(USERS[index].books, book);
+//     const final = Object.assign(USERS, update);
+//
+//     req.books = final;
+//     next();
+// }
+
 
 // Users
 app.get("/users/", getUsers, sendUsers);
 app.post("/users/", addUser, sendUsers);
-app.delete("/users/:index/");
-app.put("/users/:index/"); // (Lodash) _.merge
+app.delete("/users/:index/", deleteUsers, sendUsers);
+app.put("/users/:index/", putUsers, sendUsers);
 
 // Books
 app.get("/users/:index/books", getBooks, sendBooks);
-app.post("/users/:index/books");
+app.post("/users/:index/books", postBooks, sendBooks)
+
+
+//not done
 app.put("/users/:index/books/:title");
-app.delete("/users/:index/books/:title");
-// *
+app.delete("/users/:index/books/:title", deleteBooks, sendBooks);
 app.get("/users/:index/books/:title");
 
-// app.post("/users/")
-// app.put("/users/")
-// app.delete("/users/")
-// app.all("/users/")
+
 
 // Not Found Error
 app.use((req, res, next) => {
@@ -77,6 +121,6 @@ app.use((err, req, res, next) => {
         error: err.message,
         stack: err.stack
     })
-})
+});
 
 app.listen(config.port);
